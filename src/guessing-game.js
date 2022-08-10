7b4ab40387c64b84d4cd68aeaf78cfff34b675dc
Your Name <you@example.com>
2022-08-10 13:57:14 +0300

    init: start guessing game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.array = [];
+		this.fistNum = 0;
+        this.meanNum = 0;
+		this.lastNum = 0;
+	}
     setRange(min, max) {
-
+		for (var i = min; i < max; i++) {
+			this.array.push(i);
+		}
+		this.firstNum = 0;
+		this.lastNum = this.array.length;
     }
-
     guess() {
-
+		this.meanNum = Math.ceil((this.firstNum + this.lastNum)/2);
+		return this.array[this.meanNum];
     }
-
     lower() {
-
+		this.lastNum = this.meanNum;
     }
-
     greater() {
-
+		this.firstNum = this.meanNum;
     }
 }
 
